5bebff290e7c9a6b9bef8a176e17b7f614980861
LetiMalaga <leticiamalagutti@yahoo.com.br>
2024-07-29 15:42:59 -0300

    US11 styles: implementação inicial da tela de Menu

--- FPSARMini06/FPSARMini06/Views/Menu/MenuView.swift
+++ FPSARMini06/FPSARMini06/Views/Menu/MenuView.swift
@@ -12,18 +12,77 @@
 import SwiftUI
 
 struct MenuView: View {
+    @Binding var toggleOn: Bool
     var body: some View {
         ZStack{
-            Color.red
-                .opacity(0.5)
-                .ignoresSafeArea()
-            Text("MENUVIEW")
-                .font(.largeTitle)
-                .bold()
+            //MARK: Fundo
+            Color.red.opacity(0.2)
+            
+            VStack{
+                //MARK: Topo
+                HStack{
+                    Button{
+                        
+                    } label: {
+                        ZStack{
+                            Image("Top Bar")
+                            Image(systemName: "questionmark")
+                                .font(.system(size: 36, weight: .bold))
+                                .tint(.black)
+                        }
+                    }
+                    Spacer()
+                    Button{
+                        
+                    } label: {
+                        ZStack{
+                            Image("Top Bar")
+                            Image(systemName: "gear")
+                                .font(.system(size: 36, weight: .bold))
+                                .tint(.black)
+                        }
+                    }
+                    
+                }
+                .padding(.horizontal, 20)
+                
+                Spacer()
+                
+                Button{
+                    
+                } label: {
+                    ZStack{
+                        Image("Default")
+                        Text("PLAY")
+                            .font(.system(size: 20, weight: .bold))
+                            .tint(.black)
+                    }
+                }
+                
+                Button{
+                                    
+                } label: {
+                    ZStack{
+                        Image("Default")
+                        Text("INVENTORY")
+                            .font(.system(size: 20, weight: .bold))
+                            .tint(.black)
+                    }
+                }
+            }
+            .padding(.vertical, 20)
         }
+        .ignoresSafeArea(.all)
     }
 }
 
 #Preview {
-    MenuView()
+    struct MenuPreviewContainer: View{
+        @State private var toggleOn = false
+        
+        var body: some View {
+            MenuView(toggleOn: $toggleOn)
+        }
+    }
+    return MenuPreviewContainer()
 }
