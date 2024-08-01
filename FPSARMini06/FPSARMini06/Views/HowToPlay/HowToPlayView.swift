c08c4ceb6eb7cd28b99c5aa72bbb1d30925a6001
LetiMalaga <leticiamalagutti@yahoo.com.br>
2024-08-01 17:54:41 -0300

    US17 refactor: adição de arquivo e Geometry Reader Numbers

--- FPSARMini06/FPSARMini06/Views/HowToPlay/HowToPlayView.swift
+++ FPSARMini06/FPSARMini06/Views/HowToPlay/HowToPlayView.swift
@@ -11,56 +11,60 @@ struct HowToPlayView: View {
     @Environment(PageManager.self) var pageManager
     
     var body: some View {
-        ZStack{
-            Color.blue.opacity(0.2)
-            VStack {
-                HStack {
+        GeometryReader{ geometry in
+            ZStack{
+                Color.blue.opacity(0.2)
+                VStack {
+                    HStack {
+                        Button(action: {
+                            pageManager.page = .menu
+                        }, label: {
+                            Image("Top Bar")
+                                .resizable()
+                                .frame(width: geometry.size.width * buttonRegW, height: geometry.size.height * buttonRegH)
+                                .overlay{
+                                    Image(systemName: "chevron.left")
+                                        .font(.system(size: 36, weight: .bold))
+                                        .tint(.black)
+                                }
+                        })
+                        
+                        Spacer()
+                        
+                        Text("howToPlayView-title")
+                            .font(.system(size: 20, weight: .bold))
+                            .tint(.black)
+                            .padding(.trailing, geometry.size.width * buttonSmallW)
+                            .minimumScaleFactor(0.5)
+                        
+                        Spacer()
+                    }
+                    .padding(.vertical, 50)
+                    .padding(.horizontal, 12)
+                    
+                    Spacer()
+                    
+                    Text("LoremIpsum")
+                        .padding()
+                    
+                    Spacer()
+                    
                     Button(action: {
-                        pageManager.page = .menu
+                        pageManager.page = .gameScene
                     }, label: {
-                        Image("Top Bar")
-                            .overlay{
-                                Image(systemName: "chevron.left")
-                                    .font(.system(size: 36, weight: .bold))
+                        Image("Default")
+                            .overlay {
+                                Text("ready-button")
+                                    .font(.system(size: 20, weight: .bold))
                                     .tint(.black)
+                                    .minimumScaleFactor(0.5)
                             }
                     })
-                    
-                    Spacer()
-                    
-                    Text("howToPlayView-title")
-                        .font(.system(size: 20, weight: .bold))
-                        .tint(.black)
-                        .padding(.trailing, 56)
-                        .minimumScaleFactor(0.5)
-                    
-                    Spacer()
+                    .padding(.vertical)
                 }
-                .padding(.vertical, 50)
-                .padding(.horizontal, 12)
-                
-                Spacer()
-                
-                Text("LoremIpsum")
-                    .padding()
-                
-                Spacer()
-                
-                Button(action: {
-                    pageManager.page = .gameScene
-                }, label: {
-                    Image("Default")
-                        .overlay {
-                            Text("ready-button")
-                                .font(.system(size: 20, weight: .bold))
-                                .tint(.black)
-                                .minimumScaleFactor(0.5)
-                        }
-                })
-                .padding(.vertical)
             }
+            .ignoresSafeArea()
         }
-        .ignoresSafeArea()
     }
 }
 
