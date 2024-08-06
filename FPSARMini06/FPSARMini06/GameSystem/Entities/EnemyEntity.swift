eadf3dbe91146bd0c6d805ed812b72722b8ed2cd
Eirado <GABEIRADO2@HOTMAIL.COm>
2024-08-06 14:56:34 -0300

    model loded async inside the enemyEntity

--- FPSARMini06/FPSARMini06/GameSystem/Entities/EnemyEntity.swift
+++ FPSARMini06/FPSARMini06/GameSystem/Entities/EnemyEntity.swift
@@ -8,43 +8,55 @@
 import Foundation
 import RealityKit
 
+import Foundation
+import RealityKit
+import Combine
+
 class EnemyEntity: Entity, HasCollision, HasModel {
     
-    var model: ModelEntity
+    var model: Entity
     var animationRoot: Entity
-    var modelShape: ShapeResource // Ferramenta para definir o shape da colisao
+    var modelShape: ShapeResource // Tool to define the collision shape
     
+    private var cancellables: Set<AnyCancellable> = []
+
     required init() {
-        
-        self.model = ModelEntity()
         self.animationRoot = Entity()
         self.modelShape = .generateSphere(radius: 0.1)
         
-        self.model.components[ModelComponent.self] = ModelComponent(mesh: .generateSphere(radius: 0.1), materials: [SimpleMaterial(color: .red, isMetallic: true)])
-        
-        //Defino o comportamento de colisao aqui
-        self.model.components[gameCollisionComponent.self] = gameCollisionComponent(entityBitMask: .enemyEntityBitMask)
-        
-        self.model.generateCollisionShapes(recursive: true)
-        
-//        let extractedEntityBitMask = gameCollisionComponent(entityBitMask: .enemyEntityBitMask)
-//        let bitMask = extractedEntityBitMask.entityBitMask
-//        
-//        let entityGroup = CollisionGroup(rawValue: bitMask.rawValue)
-//        
-//        let entityMask = CollisionGroup.all.subtracting(entityGroup)
-//        
-//        self.model.collision = CollisionComponent(shapes: [modelShape], mode: .trigger, filter: .init(group: entityGroup, mask: entityMask))
+        self.model = Entity()
         
         super.init()
         
-        self.components[MotionComponent.self] = MotionComponent()
-        
-        
-        let clone = self.model.clone(recursive: true)
+        Entity.loadModelAsync()
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    print("Model loading completed.")
+                case .failure(let error):
+                    print("Failed to load model: \(error)")
+                }
+            }, receiveValue: { [weak self] assetDict in
+                guard let self = self else { return }
+                
 
-        self.addChild(clone)
-        self.addChild(self.animationRoot)
-           
+                if let modelEntity = assetDict[.diver_anim_stand_idle] {
+                    
+                    self.model = modelEntity.clone(recursive: true)
+                    
+//                    self.model.generateCollisionShapes(recursive: true)
+//                    
+//                    self.model.components[gameCollisionComponent.self] = gameCollisionComponent(entityBitMask: .enemyEntityBitMask)
+                    
+                    self.addChild(self.model)
+                }
+               
+                self.addChild(self.animationRoot)
+                
+                self.components[MotionComponent.self] = MotionComponent()
+            })
+            .store(in: &cancellables)
     }
+    
+
 }
