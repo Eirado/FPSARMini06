4b6f77d11bd53296be2674eb3a750c2e4e25d120
Giovanni Favorin de Melo <Giovannifavorim@gmail.com>
2024-08-10 09:44:49 -0300

    UI GameScene fix

--- FPSARMini06/FPSARMini06/Views/Menu/GameScene.swift
+++ FPSARMini06/FPSARMini06/Views/Menu/GameScene.swift
@@ -12,10 +12,7 @@ import SwiftData
 struct GameScene: View {
     @Environment(PageManager.self) var pageManager
     @StateObject private var gameState = GameState()
-    @State private var carregou: Bool = false
-    @State private var timeRemaining: Int = 5
-    @State private var timerRunning: Bool = false
-    
+
     @Environment (\.modelContext)  var context
     @Query private var data:[UserData]
     
@@ -28,15 +25,20 @@ struct GameScene: View {
                     startTimer()
                 }
                 .overlay {
-                    if gameState.carregou {
-                        Image("Mira")
+                    if gameState.timerRunning {
                         Image("Target")
-                        VStack{
-                            HStack{
+                        VStack {
+                            Text("Tempo restante: \(gameState.timeRemaining)")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.black.opacity(0.7))
+                                .cornerRadius(10)
+                                .padding(.top, 50)
+                            HStack {
                                 Spacer()
                                 Button(action: {
-                                    //adicionar açoes do pause
-                                    //pageManager.page = .pause
+                                    pageManager.page = .pause
                                 }, label: {
                                     Image(systemName: "pause.circle.fill")
                                         .font(.system(size: 30))
@@ -48,62 +50,33 @@ struct GameScene: View {
                             Spacer()
                         }
                     }
-                    
-                    if gameState.timerRunning {
-                        Text("Tempo restante: \(gameState.timeRemaining)")
-                            .font(.largeTitle)
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color.black.opacity(0.7))
-                            .cornerRadius(10)
-                            .padding(.top, 50)
-                    }
-                    
-                    
                 }
                 .ignoresSafeArea()
         }
     }
-        func updateScore(){
-            
-            if data.first!.score < ScoreController.score{
-                data.first?.score = ScoreController.score
-                print("score final \(String(describing: data.first?.score))")
-                do{
-                    try context.save()
-                }catch{
-                    print(error.localizedDescription)
-                }
-            }
-        }
-        func startTimer() {
-            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
-                if gameState.timeRemaining > 0 {
-                    gameState.timeRemaining -= 1
-                } else {
-                    gameState.timerRunning = false
-                    timer.invalidate()
-                    updateScore()
-                    pageManager.page = .feedbackView
-                }
+    
+    func updateScore() {
+        if data.first!.score < ScoreController.score{
+            data.first?.score = ScoreController.score
+            print("score final \(String(describing: data.first?.score))")
+            do {
+                try context.save()
+            } catch {
+                print(error.localizedDescription)
             }
         }
     }
-
-struct LoadingView: View {
-    var body: some View {
-        VStack {
-            Text("Loading...")
-                .font(.largeTitle)
-                .bold()
-                .foregroundColor(.white)
-            
-            ProgressView()
-                .progressViewStyle(CircularProgressViewStyle(tint: .white))
-                .scaleEffect(1.5)
+    
+    func startTimer() {
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            if gameState.timeRemaining > 0 {
+                gameState.timeRemaining -= 1
+            } else {
+                gameState.timerRunning = false
+                timer.invalidate()
+                updateScore()
+                pageManager.page = .feedbackView
+            }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.black.opacity(0.8))
-        .edgesIgnoringSafeArea(.all)
     }
 }
